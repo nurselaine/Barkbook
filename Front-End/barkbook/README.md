4aeecd22a953762e78212115126eda425ab9d7d1
Code-Contemporary <rsupple@protonmail.com>
2023-02-28 18:46:04 -0700

    Initial Setup
    Generated the front end structure of the application with Mantine Provider wrapping the main App component.

---
+++ Front-End/barkbook/README.md
@@ -0,0 +1,7 @@
+# Barkbook Front End
+
+## Author: Ryan Supple
+
+### Description
+
+This is the front end for the Barkbook application, built with ReactJS and Mantine styled components.
---
+++ Front-End/barkbook/src/App.js
@@ -0,0 +1,14 @@
+
+import { MantineProvider } from '@mantine/core';
+
+function App() {
+  return (
+    <MantineProvider>
+      <div className="App">
+        Woof
+      </div>
+    </MantineProvider>
+  );
+}
+
+export default App;
